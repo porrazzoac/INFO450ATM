5ccf76faf057932ab4cc42eb18303be16b7eb055
Andrew Porrazzo <porrazzoac@vcu.edu>
2019-09-19 18:09:11 -0400

    1st commit

--- INFO450ATM/INFO450ATM.cpp
+++ INFO450ATM/INFO450ATM.cpp
@@ -1,20 +1,36 @@
 // INFO450ATM.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int withdraw = 0;
+	char answer = 'y';
+	int input = 0;
+	
+	while (answer == 'Y', answer == 'y')
+
+	do// loop to accept user input 
+	{
+		cout << "please enter a withdrawl amount " << endl;
+		cin >> withdraw;
+		if (withdraw < 1)
+		{
+			cout << " the withdraw amount is invalid " << endl;
+		}
+
+		if (withdraw > 300)
+		{
+			cout << " the withdraw amount is invalid " << endl;
+		}
+
+	} while (withdraw < 1 || withdraw > 300);
+	cout << " the withdraw amount is " << " dollars." << endl;
+	cout << "Another?  type Y to continue." << endl;
+	cin >> answer;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
